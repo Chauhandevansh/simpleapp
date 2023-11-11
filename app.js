828ae0ea2f20ef3cf41ec5947b626635fa6253c2
Chauhandevansh <69097279+Chauhandevansh@users.noreply.github.com>
2023-11-11 23:08:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -84,6 +84,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://54.221.45.234:${port}`);
 });
